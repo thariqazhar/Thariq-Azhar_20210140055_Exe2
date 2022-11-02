2a5760fccef66bf0121c65e3cbd2c57c5a4eb7dc
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-02 16:52:19 +0700

    Making a case for merge sort

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -138,6 +138,33 @@ namespace Exercise2
                         P.insertionsort();
                         P.display();
                         break;
+                    case 2:
+                        Console.WriteLine("");
+                        Console.WriteLine(". . . . . . . . . . . .");
+                        Console.WriteLine("merge sort");
+                        Console.WriteLine(". . . . . . . . . . . .");
+                        Console.Write("\nProgram for sorting a numeric array using Merge Sorting");
+                        Console.Write("\n\nEnter number of elements: ");
+                        int max = Convert.ToInt32(Console.ReadLine());
+                        int[] numbers = new int[max];
+                        for (int i = 0; i < max; i++)
+                        {
+                            Console.Write("\nEnter [" + (i + 1).ToString() + "] element: ");
+                            numbers[i] = Convert.ToInt32(Console.ReadLine());
+                        }
+                        Console.Write("Input int array : ");
+                        Console.Write("\n");
+                        for (int k = 0; k < max; k++)
+                        {
+                            Console.Write(numbers[k] + " ");
+                            Console.Write("\n");
+                        }
+                        Console.WriteLine("MergeSort By Recursive Method");
+                        SortMerge(numbers, 0, max - 1);
+                        for (int i = 0; i < max; i++)
+                            Console.WriteLine(numbers[i]);
+                        Console.ReadLine();
+                        break;
                 }
                 Console.WriteLine("\n\nPress Return to exit");
                 Console.ReadLine();
