0cbb401d0eafd8d8a7b5c7aeba4d4ff879e8d11f
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-02 15:34:58 +0700

    Declaring array and variable

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2
+{
+    class Program
+    {
+        //declaring array and variable
+        private int[] Azhar = new int[30];
+        int n;
+        int i;
+    }
+}
