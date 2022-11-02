1e76daa8e25532436c4ef0d5f3810561f58fd7b0
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-11-02 15:45:24 +0700

    Creating a method for receiving input

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -14,5 +14,30 @@ namespace Exercise2
         private int[] Azhar = new int[30];
         int n;
         int i;
+
+        //creating a method for receiving input
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 30))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have a minimum of 1 and a maximum of 30 elements.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("=====================");
+            Console.WriteLine(" Enter array elements");
+            Console.WriteLine("=====================");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                Azhar[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
